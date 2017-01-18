7884ec32eb4836043af7213895cb561cd3e46a0f
baumanab <baumanab@gmail.com>
2017-01-17 22:22:27 -0800

    add bar plots of categorical (factor) variables against mean (rate) WNVPresent

--- projects/capstone/WNV_EDA.R
+++ projects/capstone/WNV_EDA.R
@@ -16,23 +16,111 @@ train_station_2 <- read.csv('train_station_2.csv') # station 2 weather only
 # convert staton to factor for use in plotting
 train$Station <- factor(train$Station)
 
-#cglimpse(train)
+glimpse(train)
 
 
 
 #distribution of temperatures via overlaid histograms
 ggplot(train, aes(x=Tmax, fill=Station)) +
-  geom_density(alpha = 0.3) +
+  geom_density(alpha= 0.2) +
+  geom_density(aes(x = blended_Tmax), alpha= 0, linetype= 'dashed') +
   my_theme()
 
 ggplot(train, aes(x=Tmin, fill=Station)) +
-  geom_density(alpha = 0.3) +
+  geom_density(alpha= 0.2) +
+  geom_density(aes(x = blended_Tmin), alpha= 0, linetype= 'dashed') +
   my_theme()
 
 ggplot(train, aes(x=Tavg, fill=Station)) +
-  geom_density(alpha = 0.3) +
+  geom_density(alpha= 0.2) +
   my_theme()
 
+ggplot(train, aes(x=PrecipTotal, fill=Station)) +
+  geom_density(alpha= 0.2) +
+  geom_density(aes(x = blended_PrecipTotal), alpha= 0, linetype= 'dashed') +
+  my_theme()
+
+ggplot(train, aes(x=PrecipTotal, fill=Station)) +
+  geom_density(alpha= 0.2) +
+  geom_density(aes(x = blended_PrecipTotal), alpha= 0, linetype= 'dashed') +
+  scale_x_log10() +
+  my_theme()
+
+ggplot(train, aes(x=Depart, fill=Station)) + # station 2 forward filled from station 1
+  geom_density(alpha= 0.2) +
+  geom_density(aes(x = blended_Depart), alpha= 0, linetype= 'dashed') +
+  my_theme()
+
+ggplot(train, aes(x=Heat, fill=Station)) +
+  geom_density(alpha= 0.2) +
+  geom_density(aes(x = blended_Heat), alpha= 0, linetype= 'dashed') +
+  my_theme()
+
+ggplot(train, aes(x=Heat, fill=Station)) +
+  geom_density(alpha= 0.2) +
+  geom_density(aes(x = blended_Tmax), alpha= 0, linetype= 'dashed') +
+  my_theme()
+
+ggplot(train, aes(x=Heat, fill=Station)) +
+  geom_density(alpha= 0.2) +
+  geom_density(aes(x = blended_Tmax), alpha= 0, linetype= 'dashed') +
+  scale_x_log10()
+  my_theme()
+
+ggplot(train, aes(x=Cool, fill=Station)) +
+  geom_density(alpha= 0.2) +
+  geom_density(aes(x = blended_Cool), alpha= 0, linetype= 'dashed') +
+  my_theme()
+
+
+
+# bar plots
+
+ggplot(train_station_1, aes(x=reorder(Block, WnvPresent, function(x){mean(x)}),
+                            y= WnvPresent)) +
+  geom_bar(stat= "summary", fun.y= "mean", color= "black", fill= 'yellow') +
+  coord_flip()
+
+ggplot(train_station_1, aes(x=reorder(Street, WnvPresent, function(x){mean(x)}),
+                            y= WnvPresent)) +
+  geom_bar(stat= "summary", fun.y= "mean", color= "black", fill= 'yellow') +
+  coord_flip()
+
+ggplot(train_station_1, aes(x=reorder(Species, WnvPresent, function(x){mean(x)}),
+                            y= WnvPresent)) +
+  geom_bar(stat= "summary", fun.y= "mean", color= "black", fill= 'yellow') +
+  coord_flip()
+
+ggplot(train_station_1, aes(x=reorder(month, WnvPresent, function(x){mean(x)}),
+                            y= WnvPresent)) +
+  geom_bar(stat= "summary", fun.y= "mean", color= "black", fill= 'yellow') +
+  coord_flip()
+
+ggplot(train_station_1, aes(x=reorder(week, WnvPresent, function(x){mean(x)}),
+                            y= WnvPresent)) +
+  geom_bar(stat= "summary", fun.y= "mean", color= "black", fill= 'yellow') +
+  coord_flip()
+
+ggplot(train_station_1, aes(x=reorder(day, WnvPresent, function(x){mean(x)}),
+                            y= WnvPresent)) +
+  geom_bar(stat= "summary", fun.y= "mean", color= "black", fill= 'yellow') +
+  coord_flip()
+
+ggplot(train_station_1, aes(x=reorder(day_of_year, WnvPresent, function(x){mean(x)}),
+                            y= WnvPresent)) +
+  geom_bar(stat= "summary", fun.y= "mean", color= "black", fill= 'yellow') +
+  coord_flip()
+
+
+
+
+
+
+# scatterplots
+
+ggplot(train, aes(x=xvar, y=yvar)) +
+  geom_point(shape=1) +    # Use hollow circles
+  geom_smooth(method=lm) 
 
 
 
